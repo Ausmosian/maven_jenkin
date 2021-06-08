7e682bdb0f9503ef8f76186661a610c3c6dc8a1b
Ausmosian <abhijeet.srivastava6499@gmail.com>
2021-06-08 13:58:51 +0530

    First Commit

---
+++ src/main/java/com/sapient/demo/MavenforjenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.sapient.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenforjenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenforjenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sapient/demo/MavenforjenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapient.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenforjenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
